6542fc65ce12187ef378919bfba2f25ba678d12a
Piyush Prateek <piyushprateek231996@gmail.com>
2023-03-04 01:51:51 +0530

    removed cors from app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { Application } from 'express';
 import { errorHandler, notFound } from '../middleware/errorMiddleware';
-// import cors from 'cors';
 const chats = require('../data/data'); //imported chats
 
 const userRoutes = require('../routes/userRoutes');
@@ -9,7 +8,6 @@ const userRoutes = require('../routes/userRoutes');
 const app: Application = express();
 
 app.use(express.json()); //to accept json data
-// app.use(cors());
 app.get('/', (req, res) => {
   res.send('Api is Running');
 });
