1223acc553484868e757f84e2bca52db76130281
Piyush Prateek <piyushprateek231996@gmail.com>
2023-03-14 22:35:26 +0530

    created sample user data

---
+++ frontend/sampledata/singleuserchat.ts
@@ -0,0 +1,28 @@
+const singleUserChat = {
+  _id: '6410960caca894e78bc9113e',
+  chatName: 'sender',
+  isGroupChat: false,
+  users: [
+    {
+      _id: '6402351271f38ba2715841c1',
+      name: 'example',
+      email: 'example@example.com',
+      pic: 'http://res.cloudinary.com/dd22mrihi/image/upload/v1677866238/v7cxxhobusdzex5gs0aw.jpg',
+      createdAt: '2023-03-03T17:57:38.324Z',
+      updatedAt: '2023-03-03T17:57:38.324Z',
+      __v: 0,
+    },
+    {
+      _id: '6402351271f38ba2715841c1',
+      name: 'example',
+      email: 'example@example.com',
+      pic: 'http://res.cloudinary.com/dd22mrihi/image/upload/v1677866238/v7cxxhobusdzex5gs0aw.jpg',
+      createdAt: '2023-03-03T17:57:38.324Z',
+      updatedAt: '2023-03-03T17:57:38.324Z',
+      __v: 0,
+    },
+  ],
+  createdAt: '2023-03-14T15:43:08.411Z',
+  updatedAt: '2023-03-14T15:43:08.411Z',
+  __v: 0,
+};
